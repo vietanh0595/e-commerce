2506c82fdfba26c917f99f252c4eb86829ce6551
VA <duongvietanh0495@gmail.com>
2023-11-19 20:59:31 +0700

    fetch products from api

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -6,20 +6,20 @@ import useStyles from './styles'
 
 const Product = ({ product }) => {
   const classes = useStyles();
+  console.log(product);
   return (
     <Card className={classes.root}>
-        <CardMedia className={classes.media} image={product.image} title={product.name} />
+        <CardMedia className={classes.media} image={product.image.url} title={product.name} />
         <CardContent>
             <div className={classes.cardContent}>
                 <Typography cariant='h5' gutterBottom>
                     {product.name}
                 </Typography>
                 <Typography cariant='h5'>
-                    {product.price}
+                    {product.price.formatted_with_symbol}
                 </Typography>
             </div>
-            <Typography variant='body2' color='textSecondary'>
-                {product.description}
+            <Typography dangerouslySetInnerHTML={{ __html: product.description}} variant='body2' color='textSecondary'>
             </Typography>
         </CardContent>
         <CardActions disableSpacing className={classes.cardActions}>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,12 +3,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'Shoes', description: "Running shoes", price: '$5', image: ''},
-    { id: 2, name: 'Shoes', description: "Running shoes", price: '$10', image: ''},
-]
-
-const Products = () => {
+const Products = ({ products }) => {
   const classes = useStyles();
 
   return (
