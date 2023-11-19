0542706501dd1f131cca463f6b330ed76e2593ef
VA <duongvietanh0495@gmail.com>
2023-11-19 17:52:16 +0700

    create product component

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import Product from './Product/Product';
+
+const products = [
+    { id: 1, name: 'Shoes', description: "Running shoes", price: '$5', image: ''},
+    { id: 2, name: 'Shoes', description: "Running shoes", price: '$10', image: ''},
+]
+
+const Products = () => {
+  return (
+    <main>
+        <Grid container justify="center" spacing={4}>
+            {products.map(product => (
+                <Grid item key={product.id} xs={12} sm={6} lg={3}>
+                    <Product product={product} />
+                </Grid>
+            ))}
+        </Grid>
+    </main>
+  )
+}
+
+export default Products
